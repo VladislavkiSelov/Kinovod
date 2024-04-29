2cb3f0a37abb0856e59fb91a71d8d312bc12346b
Vladislav Kiselov <vladislavkiselev.2007@gmail.com>
2024-04-29 23:50:57 +0300

    fix: modal auth

--- kinovod/src/components/LogInModal/LogInModal.jsx
+++ kinovod/src/components/LogInModal/LogInModal.jsx
@@ -1,14 +1,32 @@
 import React, { useRef } from "react";
+import axios from "axios";
 import style from "./LogInModal.module.scss";
 import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
 
 export default function LogInModal({ setLogInStatus, serFogotPasswordStataus, setRegistrationStatus }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const ref = useRef();
 
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
+  const passwordRegex = /^(?=.*\d).{6,}$/;
+
   const onSubmit = async (data) => {
-    console.log(data);
+    const params = { email: data.email, password: data.password };
+
+    axios
+      .get(`http://localhost:7000/user`, {
+        params,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   function handleClick(e) {
@@ -35,8 +53,22 @@ export default function LogInModal({ setLogInStatus, serFogotPasswordStataus, se
         <form className={style.logIn_form} onSubmit={handleSubmit(onSubmit)}>
           <h2>Вход</h2>
           <div className={style.box_input}>
-            <input {...register("email")} placeholder="Электронная почта" />
-            <input {...register("password")} placeholder="Пароль" />
+            <input
+              {...register("email", {
+                required: true,
+                pattern: { value: emailRegex, message: "Email неправильного формата" },
+              })}
+              placeholder="Электронная почта"
+            />
+            {errors?.email && <p className={style.error}>{errors.email.message}</p>}
+            <input
+              {...register("password", {
+                required: true,
+                pattern: { value: passwordRegex, message: "Пароль неправильного формата" },
+              })}
+              placeholder="Пароль"
+            />
+            {errors?.password && <p className={style.error}>{errors.password.message}</p>}
           </div>
           <Button text="Войти" classBtn={style.btn_logIn} />
         </form>
--- kinovod/src/components/PasswordRecovery/PasswordRecovery.jsx
+++ kinovod/src/components/PasswordRecovery/PasswordRecovery.jsx
@@ -4,8 +4,14 @@ import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
 
 export default function PasswordRecovery({ serFogotPasswordStataus, setRegistrationStatus, setLogInStatus }) {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const ref = useRef();
+
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
 
   const onSubmit = async (data) => {
     console.log(data);
@@ -35,7 +41,14 @@ export default function PasswordRecovery({ serFogotPasswordStataus, setRegistrat
         <form className={style.password_recovery_form} onSubmit={handleSubmit(onSubmit)}>
           <h2>Восстановление пароля</h2>
           <div className={style.box_input}>
-            <input {...register("email")} placeholder="Электронная почта" />
+            <input
+              {...register("email", {
+                required: true,
+                pattern: { value: emailRegex, message: "Email неправильного формата" },
+              })}
+              placeholder="Электронная почта"
+            />
+            <p className={style.error}>{errors?.email ? errors.email.message : null}</p>
           </div>
           <Button text="Отправить инструкцию" classBtn={style.btn_password_recovery} />
         </form>
